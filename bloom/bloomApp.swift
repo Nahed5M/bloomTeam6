6c5667c14735754175b6844224aa64f9bd8b79fa
Nahed5M <nahed.ksu@gmail.com>
2024-10-05 20:25:26 +0300

    Initial Commit

---
+++ bloom/bloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  bloomApp.swift
+//  bloom
+//
+//  Created by Nahed Almutairi on 02/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct bloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bloomTests/bloomTests.swift
@@ -0,0 +1,17 @@
+//
+//  bloomTests.swift
+//  bloomTests
+//
+//  Created by Nahed Almutairi on 02/04/1446 AH.
+//
+
+import Testing
+@testable import bloom
+
+struct bloomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
